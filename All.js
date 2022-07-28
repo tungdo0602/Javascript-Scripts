9934e6c8b447f82e736bad9579fe708541879d71
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-07-28 19:27:10 +0700

    Create All.js

---
+++ All.js
@@ -0,0 +1,12 @@
+function infiniteStack(n){
+    if(typeof n=='string'){
+        window[n]={}
+        if(window[n][n]){
+            window[n]
+        }else{
+            window[n][n]=window[n]
+        }
+    }else{
+        throw new TypeError('Value must be string')
+    }
+}
