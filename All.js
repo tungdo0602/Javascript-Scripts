ad0f1e69b4a96101a024dcff86eb619f34c2fbd1
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-07-28 19:33:29 +0700

    Update All.js

--- All.js
+++ All.js
@@ -29,3 +29,10 @@ function clearAllInterval(){
         clearInterval(i); 
     }
 }
+Math.random = function random(min,max,raw=false){
+    if(raw){
+        return Math.random() * (max - min + 1) / 1 + min;
+    }else{
+        return Math.floor(Math.random() * (max - min + 1) / 1 + min);
+    }
+}
