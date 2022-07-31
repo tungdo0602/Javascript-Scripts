7de30eb9fc93978949807ac4271feda899e6ca7e
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-07-31 14:40:22 +0700

    Update main.js

--- Infinite Stack Variable/main.js
+++ Infinite Stack Variable/main.js
@@ -1,9 +1,7 @@
 function infiniteStack(n){
     if(typeof n=='string'){
         window[n]={}
-        if(window[n][n]){
-            window[n]
-        }else{
+        if(!window[n][n]){
             window[n][n]=window[n]
         }
     }else{
