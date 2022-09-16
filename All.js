1cc13ec26f7833eb77eefe8998950ea7e0df4b2c
Tung <89498812+tungdo0602@users.noreply.github.com>
2022-09-16 19:28:22 +0700

    Update All.js

--- All.js
+++ All.js
@@ -55,3 +55,15 @@ function format(...code){
         return `[${formatList}m`
     }
 }
+function decodeX(v, t){
+    for(let i=0;i<t;i++){
+        v = atob(v)
+    }
+    return v
+}
+function encodeX(v, t){
+    for(let i=0;i<t;i++){
+        v = btoa(v)
+    }
+    return v
+}
